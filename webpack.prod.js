6335829df5e1d02cca1692783168ce7887452c78
dhimas-ananda <dhimas23129@ms.unesa.ac.id>
2025-11-11 14:04:39 +0700

    fix: production webpack config (output, html, copy public)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,32 @@
+const path = require('path');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',         
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/',                   
+    clean: false,                      
+  },
+
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
         ],
       },
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         exclude: /node_modules/,
         use: [
           {
@@ -28,8 +39,27 @@ module.exports = merge(common, {
       },
     ],
   },
+
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+      chunkFilename: '[id].css',
+    }),
+
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src', 'public'), to: path.resolve(__dirname, 'dist') },
+      ],
+    }),
   ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+    runtimeChunk: false,
+  },
 });
